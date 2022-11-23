f74bf827c4c1a041bb5ee2dcb9319ddbc89d7835
miketaonjo <79211048+miketaonjo@users.noreply.github.com>
2022-11-23 15:55:30 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,5 @@
 #include "philo.h"
 
-long long	ft_get_time(void)
-{
-	struct timeval	time;
-
-	if (gettimeofday(&time, NULL) == -1)
-		return (-1);
-	return ((time.tv_sec * 1000) + (time.tv_usec / 1000));
-}
-
 int	ft_philo_start(t_table *table, t_philo *philo)
 {
 	int		i;
@@ -29,56 +20,56 @@ int	ft_philo_start(t_table *table, t_philo *philo)
 	return (0);
 }
 
-int	ft_arg_init_mutex(t_table *table)
+int	ft_init_mutex(t_table *table)
 {
 	int	i;
 
-	if (pthread_mutex_init(&(table->print), NULL))
-		return (1);
-	table->fork = malloc(sizeof(pthread_mutex_t) * table->nb_philo);
-	if (!(arg->fork))
-		return (1);
+	if (pthread_mutex_init(&(table->print), NULL) != 0)
+		return (-1);
+	table->fork = malloc(sizeof(pthread_mutex_t) * table->n_philo);
+	if (!(table->fork))
+		return (-1);
 	i = 0;
-	while (i < arg->nb_philo)
+	while (table->fork[i])
 	{
-		if (pthread_mutex_init(&(table->fork[i]), NULL))
-			return (1);
+		if (pthread_mutex_init(&(table->fork[i]), NULL) != 0)
+			return (-1);
 		i++;
 	}
 	return (0);
 }
 
-int	ft_arg_init(t_table *table, int argc, char **argv)
+int	ft_init_table(t_table *table, int argc, char **argv)
 {
-	table->nb_philo = ft_atoi(argv[1]);
+	struct timeval	tv;
+	
+	table->n_philo = ft_atoi(argv[1]);
 	table->time_to_die = ft_atoi(argv[2]);
 	table->time_to_eat = ft_atoi(argv[3]);
 	table->time_to_sleep = ft_atoi(argv[4]);
-	table->start_time = ft_get_time();
-	if (table->nb_philo <= 0 || table->time_to_die < 0 || table->time_to_eat < 0
-		|| table->time_to_sleep < 0)
-	{
-		return (5);
-	}
 	if (argc == 6)
-	{
-		table->eat_times = ft_atoi(argv[5]);
-		if (table->eat_times <= 0)
-			return (6);
-	}
-	if (ft_arg_init_mutex(table))
-		return (1);
+		table->n_meal = ft_atoi(argv[5]);
+	if (table->n_philo <= 0 || table->time_to_die < 0 || table->time_to_eat < 0
+		|| table->time_to_sleep < 0 || table->n_meal <= 0)
+		return (-1);
+	if (gettimeofday(&tv, NULL) == -1)
+		putstr_error("Error timeval");
+	table->start = (tv.tv_sec * 1000) + (tv.tv_usec / 1000);
+	table->stop = -1;
+	table->finish_eat = -1;
+	if (ft_init_mutex(table) == -1)
+		return (-1);
 	return (0);
 }
 
-int	ft_philo_init(t_philo **philo, t_table *table)
+int	ft_init_philo(t_philo *philo, t_table *table)
 {
 	int	i;
 
 	i = 0;
-	*philo = malloc(sizeof(t_philo) * table->nb_philo);
+	*philo = malloc(sizeof(t_philo) * table->n_philo);
 	if (!(philo))
-		return (1);
+		return (-1);
 	while (i < table->nb_philo)
 	{
 		(*philo)[i].table = table;
@@ -95,20 +86,16 @@ int	ft_philo_init(t_philo **philo, t_table *table)
 int	main(int argc, char **argv)
 {
 	t_table	table;
-	t_philo	*philo;
-	int		errno;
+	t_philo	philo;
 
 	if (argc != 5 && argc != 6)
-		return (print_error("error argc", 3));
-	memset(&table, 0, sizeof(t_table));
-	errno = ft_arg_init(&table, argc, argv);
-	if (errno)
-		return (print_error("error arg init", errno));
-	errno = ft_philo_init(&philo, &table);
-	if (errno)
-		return (print_error("error philo init", errno));
-	errno = ft_philo_start(&table, philo);
-	if (errno)
+		return (putstr_error("Error argc", 3));
+	if (ft_init_table(&table, argc, argv) != 0)
+		putstr_error("Error init table"));
+	if (ft_init_philo(&philo, &table) != 0)
+		putstr_error("Error init philo"));
+	ret = ft_philo_start(&table, philo);
+	if (ret)
 		return (print_error("error philo start", errno));
 	return (0);
 }
