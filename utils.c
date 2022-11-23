7ba14b3b35785d6e7e7e8c981c3dbfcfb1b5ffc0
miketaonjo <79211048+miketaonjo@users.noreply.github.com>
2022-11-23 17:42:11 +0100

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,4 +1,4 @@
-void	ft_free_tab(t_table *table)
+void	ft_free_table(t_table *table)
 {
 	int	i;
 	
@@ -10,4 +10,28 @@ void	ft_free_tab(t_table *table)
 		free(table-<fork);
 		table->fork = NULL;
 	}
+}
+
+int	ft_atoll(char *s)
+{
+	long long	result;
+	int		sign;
+
+	result = 0;
+	sign = 0;
+	while (*s == '\t' || *s == '\n' || *s == '\v'
+		|| *s == '\f' || *s == '\r' || *s == ' ')
+		s++;
+	if (*s == '-')
+		sign = -1;
+	if (*s == '+' || *s == '-')
+		s++;
+	while (*s >= '0' && *s <= '9')
+	{
+		result = result * 10 + (*s - 48);
+		if (result < 0)
+			return (-1);
+		s++;
+	}
+	return (result * sign);
 }
