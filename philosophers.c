24739d3576866015a8c79df5057b976f39b2b2f2
miketaonjo <79211048+miketaonjo@users.noreply.github.com>
2022-11-23 18:35:45 +0100

    Update philosophers.c

--- philosophers.c
+++ philosophers.c
@@ -1,13 +1,41 @@
-int	ft_start(t_table *table, t_philo *philo)
+void	ft_thread(void *arg)
+{
+	if (arg->id % 2 = 0)
+	{
+		if (usleep(500) != 0)
+		{
+			ft_free_philos(table, philo);
+			return ;
+		}
+	}
+	else
+	{
+		if (usleep(1000) != 0)
+		{
+			ft_free_philos(table, philo);
+			return ;
+		}
+	}
+}
+
+int	ft_start_thread(t_table *table, t_philo *philo)
 {
 	int		i;
+	struct timeval tv;
 
 	i = 0;
 	while (i < table->nb_philo)
-	{	
-		philo[i].lst_eat = ft_get_time();
-		if (pthread_create(&(philo[i].thread), NULL, ft_thread, &(philo[i])))
-			return (1);
+	{
+		if (gettimeofday(&tv, NULL) == -1)
+		{
+			putstr_error("Error timeval");
+			return (-1);
+		}
+		philo[i].lst_eat = (tv.tv_sec * 1000) + (tv.tv_usec / 1000);
+		if (pthread_create(&(philo[i].thread), NULL, ft_thread, &(philo[i])) != 0)
+		{
+			ft_free_philos(table, philo);
+		}
 		i++;
 	}
 	ft_philo_check_finish(table, philo);
