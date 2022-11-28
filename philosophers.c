adad91204d6bbf29ec72da1315a77309013c4ce2
miketaonjo <79211048+miketaonjo@users.noreply.github.com>
2022-11-28 12:28:27 +0100

    Update philosophers.c

--- philosophers.c
+++ philosophers.c
@@ -1,3 +1,70 @@
+void	*ft_thread(void *argv)
+{
+	t_arg		*arg;
+	t_philo		*philo;
+
+	philo = argv;
+	arg = philo->arg;
+	if (philo->id % 2)
+		usleep(1000);
+	else
+		usleep(500);
+	while (!arg->finish)
+	{
+		ft_philo_action(arg, philo);
+		if (arg->eat_times == philo->eat_count)
+		{
+			arg->finished_eat++;
+			break ;
+		}
+		ft_philo_printf(arg, philo->id, "is sleeping");
+		ft_pass_time((long long)arg->time_to_sleep, arg);
+		ft_philo_printf(arg, philo->id, "is thinking");
+	}
+	return (0);
+}
+
+void	ft_free_thread(t_arg *arg, t_philo *philo)
+{
+	int	i;
+
+	i = 0;
+	while (i < arg->philo_num)
+		pthread_mutex_destroy(&(arg->forks[i++]));
+	free(philo);
+	free(arg->forks);
+	pthread_mutex_destroy(&(arg->print));
+}
+
+void	ft_philo_check_finish(t_arg *arg, t_philo *philo)
+{
+	int			i;
+	long long	now;
+
+	while (!arg->finish)
+	{
+		if ((arg->eat_times != 0) && (arg->philo_num == arg->finished_eat))
+		{
+			arg->finish = 1;
+			break ;
+		}
+		i = 0;
+		while (i < arg->philo_num)
+		{
+			now = ft_get_time();
+			if ((now - philo[i].last_eat_time) >= arg->time_to_die)
+			{
+				ft_philo_printf(arg, i, "died");
+				arg->finish = 1;
+				break ;
+			}
+			i++;
+		}
+	}
+}
+
+
+
 void	ft_thread(void *arg)
 {
 	if (arg->id % 2 = 0)
